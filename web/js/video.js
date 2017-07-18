cdedc3750c124684d4d162d8d7699866794ec45a
Steven Mazliach <smazliac@ucsc.edu>
2017-07-17 21:05:59 -0700

    trying alternate way to use Vue try4

--- web/js/video.js
+++ web/js/video.js
@@ -1,16 +1,21 @@
 
-Vue.component('chat-item', {
-  props: ['message'],
-  template: '<li>{{ message.text }}</li>'
-})
+setTimeout(function(){ 
 
-var chatbox = new Vue({
-  el: '#chatbox',
-  data: {
-    chats_arr: [
-    	{ id: 0, text: 'Vegetables' },
-      	{ id: 1, text: 'Cheese' },
-      	{ id: 2, text: 'Whatever else humans are supposed to eat' }
-    ]
-  }
-})
+	Vue.component('chat-item', {
+	  props: ['message'],
+	  template: '<li>{{ message.text }}</li>'
+	})
+
+	var chatbox = new Vue({
+	  el: '#chatbox',
+	  data: {
+	    chats_arr: [
+	    	{ id: 0, text: 'Vegetables' },
+	      	{ id: 1, text: 'Cheese' },
+	      	{ id: 2, text: 'Whatever else humans are supposed to eat' }
+	    ]
+	  }
+	})
+
+}, 3000);
+
