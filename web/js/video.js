d390e7d762d7bb999cef384545301a101562ada6
Steven Mazliach <smazliac@ucsc.edu>
2017-07-17 22:16:06 -0700

    trying to have Vue collect json from chats

--- web/js/video.js
+++ web/js/video.js
@@ -9,13 +9,13 @@ setTimeout(function(){
 	var chatbox = new Vue({
 	  el: '#chatbox',
 	  data: {
-	    chats_arr: [
-	    	{ id: 0, text: 'Vegetables' },
-	      	{ id: 1, text: 'Cheese' },
-	      	{ id: 2, text: 'Whatever else humans are supposed to eat' }
-	    ]
+	    chats_arr: []
 	  }
 	})
+
+	$.get('/scrape/MFH0i0KcE_o').then(function(responseData) {
+  		chatbox.chats_arr = responseData;
+	});
 
 }, 2000);
 
